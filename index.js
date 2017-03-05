4a5eabc09b8e34075bba5826194e54640d4a9884
Simen Bekkhus <sbekkhus91@gmail.com>
2017-03-05 15:51:54 +0100

    Upgrade lint

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 module.exports = {
   plugins: [
     require('babel-plugin-transform-es3-member-expression-literals'),
-    require('babel-plugin-transform-es3-property-literals')
-  ]
+    require('babel-plugin-transform-es3-property-literals'),
+  ],
 };
